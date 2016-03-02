f3bc7ea9b87b50dadb98ffb04d21ff91204df90e
leandroveiga <lean.veiga@gmail.com>
2016-03-02 07:53:55 -0300

    commit

---
+++ cd-cities-ui/src/main/java/com/example/cities/controller/CitiesController.java
@@ -0,0 +1,62 @@
+package com.example.cities.controller;
+
+//
+import com.example.cities.client.model.FormInput;
+import com.example.cities.client.model.PagedCities;
+import com.example.cities.client.repositories.CityRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CitiesController {
+    private CityRepository repository;
+
+    @Autowired
+    public CitiesController(CityRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String index(Model uiModel,Pageable pageable) {
+    	PagedCities cities = list(uiModel,pageable);
+    	uiModel.addAttribute("pagedCities",cities);
+    	uiModel.addAttribute("formInput",new FormInput("",cities.getMetadata().getSize()));
+        return "index";
+    }
+    @RequestMapping(value="/", params={"name","size"}, method = RequestMethod.GET)
+    public String search(@RequestParam("name") String name, @RequestParam("size") Long size, Model uiModel,Pageable pageable) {
+    	PagedCities cities = search(name,pageable);
+    	uiModel.addAttribute("pagedCities",cities);
+    	FormInput input = new FormInput(name,size);
+    	uiModel.addAttribute("formInput",input);
+        return "index";
+    }
+    
+
+    @RequestMapping(value="/cities/", method = RequestMethod.GET)
+    public PagedCities list(Model uiModel,Pageable pageable) {
+    	PagedCities cities = repository.findAll(pageable.getPageNumber(), pageable.getPageSize());
+    	uiModel.addAttribute("pagedCities",cities);
+        return cities;
+    }
+
+    @RequestMapping(value = "/cities/search", method = RequestMethod.GET)
+    public PagedCities search(@RequestParam("name") String name, Pageable pageable) {
+        return repository.findByNameContains(name, pageable.getPageNumber(), pageable.getPageSize());
+    }
+    
+     @RequestMapping(value="/loaderio-8f1a7ed85cf39a6750679a2a5abf761a.html", method = RequestMethod.GET)
+     @ResponseBody
+    public String loader() {
+        return "loaderio-8f1a7ed85cf39a6750679a2a5abf761a";
+    }
+   
+    
+}
